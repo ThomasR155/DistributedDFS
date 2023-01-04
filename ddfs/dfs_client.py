fdc2687fc2cd4d5019754bffa780844559baf638
ThomasR155 <Thomas_Riedl@gmx.net>
2023-01-04 17:28:02 +0100

    final run

--- ddfs/dfs_client.py
+++ ddfs/dfs_client.py
@@ -17,11 +17,7 @@ print("Root: ", global_node_id)
 
 
 def run():
-    with open("ip_configuration.yml",'r') as file_ip:
-        dict_ip = yaml.safe_load(file_ip)
-    #ip =str(dict_ip.get(str(global_node_id)))+':'+str(PORT)
     ip ='localhost:'+str(PORT)
-
     #make root DFS procedure 
     edges = np.empty((0,2), int)
     with grpc.insecure_channel(ip) as channel:
@@ -29,16 +25,15 @@ def run():
         response = stub.MakeRoot(dfs_pb2.RootRequest(type=1))
         for i in range(0, len(response.child)):
             print("Child: " + str(response.child[i]) + " Parent: " + str(response.parent[i]))
+            #get edges from the parent/child pair
             edge = np.sort(np.array([response.parent[i], response.child[i]]))
-            edges=np.append(edges,np.array([edge]), axis=0)
-            edges = np.unique(edges, axis=0)
-                 
+            edges=np.append(edges,np.array([edge]), axis=0)           
     G = nx.from_edgelist(edges)
     pos = nx.spring_layout(G)
     color_map = ['red' if node == global_node_id else 'green' for node in G] 
     nx.draw(G, pos, node_size=1500, node_color=color_map, font_size=10, font_weight='bold',with_labels=True)
+    # save spanning tree
     plt.savefig("output/spanning_tree_root_{}.png".format(int(global_node_id)))
-
     with grpc.insecure_channel(ip) as channel:
         stub = dfs_pb2_grpc.DFSStub(channel)
         response = stub.ResetNetworkSvc(dfs_pb2.ResetNetworkRequest(type=1))
--- ddfs/dfs_containerization.py
+++ ddfs/dfs_containerization.py
@@ -4,24 +4,29 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 def plot_input_graph(edge_list):
-    edges = np.unique(edge_list, axis=1)
-    G = nx.from_edgelist(edges)
+    G = nx.from_edgelist(edge_list)
     pos = nx.spring_layout(G)
-    nx.draw(G, pos, node_size=1500, node_color='yellow', font_size=8, font_weight='bold',with_labels=True)
+    nx.draw(G, pos, node_size=1500, node_color='yellow', font_size=14, font_weight='bold',with_labels=True)
     plt.savefig("../output/input_graph.png")
 
 def get_neigbours_yaml(edge_list):
     neighbours = {}
+    # all unique numbers in the edge list are vertices
     vertices = np.unique(edge_list)
-    for vertix in vertices:
+    # for all vertices look for the neighbours
+    for vertex in vertices:
         neighbours_v = []
+        # iterate through all edges to check for neighbours
         for edge in edge_list:
-            if np.any(edge ==vertix):
-                neighbour=int(edge[int(np.where(edge != vertix)[0])])
+            # if vertix is part of edge, the other part of the edge is a neighbour
+            if np.any(edge ==vertex):
+                neighbour=int(edge[int(np.where(edge != vertex)[0])])
                 if neighbour not in neighbours_v:
                     neighbours_v.append(neighbour)
+        # sort the neighbours descending for and ordered spanning tree in the end
         neighbours_v.sort(reverse=True)
-        neighbours[int(vertix)] = neighbours_v
+        neighbours[int(vertex)] = neighbours_v
+    # save neighbours to yml file
     with open('neighbours.yml', 'w') as outfile:
         yaml.dump(neighbours, outfile, default_flow_style=False)
     print("direct neighbours for each node:")
@@ -29,12 +34,15 @@ def get_neigbours_yaml(edge_list):
     
 
 def write_docker_compose_yml(edge_list,port):
+    # all unique numbers in the edge list are vertices
     vertices = np.unique(edge_list)
     compose_file = {}
     compose_file["version"]="3"
+    # all nodes are specified inside of services:
     services = {}
     with open("ip_configuration.yml",'r') as file_ip:
         dict_ip = yaml.safe_load(file_ip)
+    # use ip configuration
     for v in vertices:
         node=       {"build":
                         {"context": ".",
@@ -49,14 +57,18 @@ def write_docker_compose_yml(edge_list,port):
                     }
         services["n{}".format(int(v))]=node
 
+    # all nodes are done
     compose_file["services"]= services
+
+    # configure subnet network
     compose_file["networks"]= {"frontend":
                                     {"ipam":
                                         {"config":
                                             [{"subnet":dict_ip.get("subnet")}],
                                         }
                                     }
-                                }                
+                                }      
+    #save the compose file          
     with open('../docker-compose.yml', 'w') as outfile:
         yaml.dump(compose_file, outfile, default_flow_style=False)
     print("Initialization of Node 1:")
